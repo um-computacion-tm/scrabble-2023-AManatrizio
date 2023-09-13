907a90e1342b4a0f501c87c99367977128caf77d
AMnatrizio <antonellamanatrizio@hotmail.com>
2023-09-12 23:45:15 -0300

    agrego funcion next_turn para los test: test_next_turn_when_player_is_last y test_next_turn_when_player_is_not_the_first

--- test/test_scrabble_game.py
+++ test/test_scrabble_game.py
@@ -3,6 +3,7 @@ from game.scrabble import ScrabbleGame
 
 
 class TestScrabbleGame(unittest.TestCase):
+    
     def test_init(self):
         scrabble_game = ScrabbleGame(players_count=3)
         self.assertIsNotNone(scrabble_game.board)
@@ -13,30 +14,26 @@ class TestScrabbleGame(unittest.TestCase):
         self.assertIsNotNone(scrabble_game.bag_tiles)
 
 
+    def test_next_turn_when_game_is_starting(self):
+        #validar que al comienzo, el turno es del jugador 1
+         scrabble_game = ScrabbleGame(players_count=3)
 
-    # def test_next_turn_when_game_is_starting(self):
-    #     #validar que al comienzo, el turno es del jugador 1
-    #      scrabble_game = ScrabbleGame(players_count=3)
-
-    #      assert scrabble_game.current_player == scrabble_game.players[0]
-
+         assert scrabble_game.current_player == scrabble_game.players[0]
 
 
-    # def test_next_turn_when_player_is_not_the_first(self):
-    #     #Validar que luego del jugador 0, le toca al jugador 1
-    #     scrabble_game = ScrabbleGame(players_count=3)
-    #     scrabble_game.current_player = scrabble_game.players[0]
-    #     scrabble_game.next_turn()
-    #     assert scrabble_game.current_player == scrabble_game.players[1]
+    def test_next_turn_when_player_is_not_the_first(self):
+        #Validar que luego del jugador 0, le toca al jugador 1
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[0]
+        scrabble_game.next_turn()
+        assert scrabble_game.current_player == scrabble_game.players[1]
 
-    # def test_next_turn_when_player_is_last(self):
-    #     #Suponiendo que tenemos 3 jugadores, luego del jugador 3, le toca al jugador 1
-    #     scrabble_game = ScrabbleGame(players_count=3)
-    #     scrabble_game.current_player = scrabble_game.players[2]
-    #     scrabble_game.next_turn()
-    #     assert scrabble_game.current_player == scrabble_game.players[0]
-
-
+    def test_next_turn_when_player_is_last(self):
+        #Suponiendo que tenemos 3 jugadores, luego del jugador 3, le toca al jugador 1
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[2]
+        scrabble_game.next_turn()
+        assert scrabble_game.current_player == scrabble_game.players[0]
 
 
 
