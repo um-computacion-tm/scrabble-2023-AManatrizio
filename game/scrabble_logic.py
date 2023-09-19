2da50de13135f220d8c0b0d59655b924df3bf78f
AMnatrizio <antonellamanatrizio@hotmail.com>
2023-09-19 20:44:45 -0300

    cambios en la funcion calculate_word_value que funciona para cuando el multiplicador no esta activo

--- game/scrabble_logic.py
+++ game/scrabble_logic.py
@@ -1,22 +1,45 @@
 from game.cell import Cell
+from game.models import Tile
 
-def calculate_word_value(word):
-    total_value = 0
-    word_multiplier = 1
+class ScrabbleLogic:
+    @staticmethod
+    def calculate_word_value(word):
+        # Inicializar las variables para el valor total y el multiplicador de palabra
+        total_value = 0
+        word_multiplier = 1
 
-    for cell in word:
-        cell_value = 0
+        # Inicializar una bandera para rastrear si hay un multiplicador de letra activo
+        letter_multiplier_active = False
 
-        if cell.letter is not None:
-            cell_value = cell.letter.value
+        # Recorrer cada celda en la palabra
+        for cell in word:
+            cell_value = 0
 
-            if cell.multiplier_type == 'letter':
-                cell_value *= cell.multiplier
-            elif cell.multiplier_type == 'word':
-                word_multiplier *= cell.multiplier
+            # Si la celda contiene una letra
+            if cell.letter is not None:
+                cell_value = cell.letter.value
 
-        total_value += cell_value
+                # Verificar el tipo de multiplicador (letra o palabra)
+                if cell.multiplier_type == 'letter':
+                    if cell.multiplier_active:
+                        cell_value *= cell.multiplier
+                    else:
+                        # Si el multiplicador de letra no está activo, indicarlo
+                        letter_multiplier_active = True
+                elif cell.multiplier_type == 'word':
+                    # Aplicar el multiplicador de palabra
+                    word_multiplier *= cell.multiplier
 
-    return total_value * word_multiplier
+            # Agregar el valor de la celda al valor total
+            total_value += cell_value
+
+        # Si no hay multiplicador de letra activo, aplicar el multiplicador de palabra al valor total
+        if not letter_multiplier_active:
+            total_value *= word_multiplier
+
+        # Devolver el valor total de la palabra
+        return total_value
+
+
 
 
