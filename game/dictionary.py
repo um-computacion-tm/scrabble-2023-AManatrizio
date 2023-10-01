ff40bb7ad671fa07916ab7dbbcdca312acc13dd0
AMnatrizio <antonellamanatrizio@hotmail.com>
2023-10-01 14:58:06 -0300

    cambio de nombre el archivo word_validator por dictionary

---
+++ game/dictionary.py
@@ -0,0 +1,27 @@
+#En esta clase se importa el diccionario de la API, para validar las palabras
+
+from pyrae import dle
+
+class Dictionary:
+
+    #constructor
+    def __init__(self):
+        pass 
+
+    def validate_word(self, palabra):
+        # Lógica para validar la palabra utilizando la API pyrae
+        try:
+            # Realizar una búsqueda en la API
+            resultado = dle.search_by_word(word=palabra)
+            # Procesar el resultado y determinar si la palabra es válida
+            es_valida = self.procesar_resultado(resultado)
+            return es_valida
+        except Exception as e:
+            print(f"Error al validar la palabra: {e}")
+            return False
+
+    def procesar_resultado(self, resultado):
+        # Aquí puedes analizar el resultado de la búsqueda y determinar si la palabra es válida
+        # Por ejemplo, puedes verificar si la palabra tiene una definición en el diccionario
+        definiciones = resultado.get_definitions()
+        return bool(definiciones)
