6464b85cab9aab73ac512ffab8735ca0ebd6fb73
AMnatrizio <antonellamanatrizio@hotmail.com>
2023-10-09 22:47:39 -0300

    test para la funcion validate_words

--- test/test_scrabble_game.py
+++ test/test_scrabble_game.py
@@ -36,6 +36,34 @@ class TestScrabbleGame(unittest.TestCase):
         assert scrabble_game.current_player == scrabble_game.players[0]
 
 
+    def test_validate_word(self):
+        # Crear una instancia de ScrabbleGame con un jugador
+        scrabble_game = ScrabbleGame(players_count=1)
+
+        # Configurar el tablero para que la palabra "CASA" encaje horizontalmente en la posición (0, 0)
+        scrabble_game.board.place_tile('C', (0, 0))
+        scrabble_game.board.place_tile('A', (0, 1))
+        scrabble_game.board.place_tile('S', (0, 2))
+        scrabble_game.board.place_tile('A', (0, 3))
+
+        # Validar la palabra "CASA" en la ubicación (0, 0) horizontalmente
+        word = "CASA"
+        location = (0, 0)
+        orientation = "horizontal"
+        
+        # Crear una lista de fichas que representan las letras de la palabra
+        word_tiles = [Tile(letter) for letter in word]
+
+        # Configurar la mano del jugador con algunas letras
+        player = scrabble_game.current_player
+        player.tiles = ['A', 'C', 'S', 'A', 'E', 'F']
+
+        # Validar la palabra con las letras del jugador usando el método has_letters
+        result = player.has_letters(word_tiles)
+
+        # Verificar que la palabra sea válida
+        self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()
